3b80ba172afce335c1e6d6ff5a5aab6a62f0c226
Hikaru Wada <watyaa@gmail.com>
2021-10-10 16:25:11 +0900

    setup

---
+++ src/Code.ts
@@ -0,0 +1,7 @@
+// Compiled using ring 1.0.0 (TypeScript 4.4.3)
+function myFunction() {
+    var greeting = "hello, GAS";
+    Logger.log(greeting);
+    var aisatsu = "こんにちは、GAS";
+    console.log(aisatsu);
+}
