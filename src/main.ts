6bfeb06e79e2e25e5a0c7d0b22d91a755a594c19
Hikaru Wada <watyaa@gmail.com>
2021-10-24 01:15:40 +0900

    fix ordering bug

--- src/main.ts
+++ src/main.ts
@@ -18,17 +18,21 @@ export const ringFitAdventure = () => {
   const tweets = getTweetsWithMediaUrl(properties.TW_TARGET_IDS.split(","), {
     start_time: properties.LAST_RUN_AT,
     end_time: now.toISOString(),
+  })?.sort((a, b) => {
+    if (a.created_at < b.created_at) return -1
+    if (a.created_at > b.created_at) return 1
+    return 0
   })
   if (!tweets) return
   // ツイート内の画像をOCRして名前・活動時間・消費カロリー・走行距離を取得
   const ocrResult = doOCR(tweets.map((t) => t.attachments[0].url))
   if (!ocrResult) return
-  // Sheet の行に日付とOCRの結果レコードを追加
   const texts = ocrResult.responses.map(
     (res) => res.textAnnotations[0] as OCRFirstText
   )
   // For DEBUG
   texts.map((t) => console.log("OCR description: ", t.description))
+  // Sheet の行に日付とOCRの結果レコードを追加
   doRecording(
     properties.SHEET_ID,
     texts
