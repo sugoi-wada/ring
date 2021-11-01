051ddd151c536d432ea4f88977b43971239e7f9f
Hikaru Wada <watyaa@gmail.com>
2021-11-02 01:04:31 +0900

    fix message

--- src/message.ts
+++ src/message.ts
@@ -16,5 +16,5 @@ const fotmatDuration = (d: FitnessStat["totalFitnessDuration"]) => {
   const h = d.hours > 0 ? `${d.hours}時間` : null
   const m = d.minutes > 0 ? `${d.minutes}分` : null
   const s = d.seconds > 0 ? `${d.seconds}秒` : null
-  return [h, m, s].filter(isPresent)
+  return [h, m, s].filter(isPresent).join("")
 }
